ac1e45b50ebb9bc148cecb3f58bdec57ff629bcf
Taylor Owen <towenmilner@gmail.com>
2024-11-21 14:25:51 -0800

    Added attributes and an actor which applies effects

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -3,3 +3,27 @@
 
 #include "AbilitySystem/AuraAttributeSet.h"
 
+#include "Net/UnrealNetwork.h"
+
+UAuraAttributeSet::UAuraAttributeSet()
+{
+	InitHealth(100.f);
+	InitMaxHealth(100.f);
+	InitMana(10.f);
+	InitMaxMana(10.f);
+}
+
+void UAuraAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, Mana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+}
+
+GAMEPLAY_ATTRIBUTE_DEF(UAuraAttributeSet, Health)
+GAMEPLAY_ATTRIBUTE_DEF(UAuraAttributeSet, MaxHealth)
+GAMEPLAY_ATTRIBUTE_DEF(UAuraAttributeSet, Mana)
+GAMEPLAY_ATTRIBUTE_DEF(UAuraAttributeSet, MaxMana)
--- Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -3,8 +3,33 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
 #include "AttributeSet.h"
 #include "AuraAttributeSet.generated.h"
+
+/**
+ * NOTE: This macro will safely implement the glue code needed to declare and replicate a gameplay attribute.
+ *	Unfortunately, Unreal Engine will not recognize the attribute as a UPROPERTY if you wrap it in a macro like this.
+ *	Make sure to expand this macro in the header file where you declare the attribute.
+ */
+#define GAMEPLAY_ATTRIBUTE_DECL(AttrName, CategoryName) \
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_##AttrName, Category = CategoryName) \
+	FGameplayAttributeData AttrName; \
+	UFUNCTION() \
+	void OnRep_##AttrName(const FGameplayAttributeData& Old##AttrName);
+
+// This macro is fine, as far as I can tell
+#define GAMEPLAY_ATTRIBUTE_DEF(ClassName, AttrName) \
+	void ClassName##::OnRep_##AttrName(const FGameplayAttributeData& Old##AttrName) \
+	{ \
+		GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, AttrName, Old##AttrName); \
+	}
+
+#define GAMEPLAY_ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
 /**
  * 
@@ -14,4 +39,24 @@ class AURA_API UAuraAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
 	
+public:
+	UAuraAttributeSet();
+	
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	// GAMEPLAY_ATTRIBUTE_DECL(Health, "Vital Attributes")
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Vital Attributes") FGameplayAttributeData Health; UFUNCTION() void OnRep_Health(const FGameplayAttributeData& OldHealth);
+	GAMEPLAY_ATTRIBUTE_ACCESSORS(UAuraAttributeSet, Health)
+	
+	// GAMEPLAY_ATTRIBUTE_DECL(MaxHealth, "Vital Attributes")
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth, Category = "Vital Attributes") FGameplayAttributeData MaxHealth; UFUNCTION() void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth);
+	GAMEPLAY_ATTRIBUTE_ACCESSORS(UAuraAttributeSet, MaxHealth)
+	
+	// GAMEPLAY_ATTRIBUTE_DECL(Mana, "Vital Attributes")
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Mana, Category = "Vital Attributes") FGameplayAttributeData Mana; UFUNCTION() void OnRep_Mana(const FGameplayAttributeData& OldMana);
+	GAMEPLAY_ATTRIBUTE_ACCESSORS(UAuraAttributeSet, Mana)
+	
+	// GAMEPLAY_ATTRIBUTE_DECL(MaxMana, "Vital Attributes")
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxMana, Category = "Vital Attributes") FGameplayAttributeData MaxMana; UFUNCTION() void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana);
+	GAMEPLAY_ATTRIBUTE_ACCESSORS(UAuraAttributeSet, MaxMana)
 };
