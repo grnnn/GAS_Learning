7f623cb41dd4df1493377122a8baccfc40b8c83a
Taylor Owen <towenmilner@gmail.com>
2025-02-07 14:49:40 -0800

    Added elemental damage types and corresponding set-able resistances with corresponding attributes

---
+++ Source/Aura/Public/AbilitySystem/Ability/AuraDamageGameplayAbility.h
@@ -0,0 +1,23 @@
+// Copyright Taylor Owen-Milner
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Ability/AuraGameplayAbility.h"
+#include "AuraDamageGameplayAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraDamageGameplayAbility : public UAuraGameplayAbility
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TSubclassOf<UGameplayEffect> DamageEffectClass;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Damage")
+	TMap<FGameplayTag, FScalableFloat> DamageModifiers;
+};
